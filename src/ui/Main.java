492aeae3f8bec1b6f7c8c3b9fee93656abd709a0
Daniela Bonilla Caceres <danielabc_0106@hotmail.com>
2021-03-10 09:33:15 -0500

    Second commit: "I made the method findBooks which use the binary search to find the bookss that Peter can pay"

--- src/ui/Main.java
+++ src/ui/Main.java
@@ -12,13 +12,17 @@ import java.util.List;
 
 public class Main {
 	
+	public static List<String> output;
+	public static List<Integer> booksPrices;
 	public final static String SPLIT=" ";
 	public final static BufferedReader br = new BufferedReader (new InputStreamReader(System.in));
 	public final static BufferedWriter bw = new BufferedWriter (new OutputStreamWriter(System.out));
 
 	public static void main(String[] args) throws IOException {
+		output= new ArrayList<String>();
+		booksPrices =new ArrayList<Integer>();
 		
-		bw.write("indique la cantidad de libros disponibles");
+		//bw.write("indique la cantidad de libros disponibles");
 		bw.flush();
 		String books=br.readLine();
 		
@@ -28,9 +32,12 @@ public class Main {
 		
 		receiveBooksPrices();
 		
-		bw.write("indique la cantidad de dinero disponibles");
+		//bw.write("indique la cantidad de dinero disponibles");
 		bw.flush();
-		int money = Integer.parseInt(br.readLine());
+		findBooks();
+		
+		br.close();
+		bw.close();
 	}
 	
 	
@@ -42,6 +49,7 @@ public class Main {
 			if (line!=null) {
 				String []parts= line.split(SPLIT);
 				stringToInteger(parts);
+				System.out.println("Lo puse en el array de String");
 			}		
 			
 		}catch (IOException e){
@@ -55,7 +63,7 @@ public class Main {
 			Integer []bPrices = new Integer [words.length];
 			for (int i=0;i<bPrices.length;i++) {
 				bPrices[i]=Integer.parseInt(words[i]);
-				//System.out.println("string to integer: "+bPrices[i]);				
+				System.out.println("string to integer: "+bPrices[i]);				
 			}
 			
 			sortPrices(bPrices);
@@ -69,8 +77,123 @@ public class Main {
 		prices = Arrays.asList(bookPrices);
 		
 		Collections.sort(prices);
-		//System.out.println(Arrays.toString(prices.toArray()));	 
+		booksPrices.clear();
+		for (int i=0;i<prices.size();i++) {
+			booksPrices.add(prices.get(i));
+		}		
 		
+		System.out.println(Arrays.toString(prices.toArray()));		
 	}
+	
+	
+	/*
+	 * Me falta el método de busqueda binaria
+	 * la salida
+	 * comprobar si funciona
+	 */
+	
+	public static void findBooks() throws IOException{
+		int money = Integer.parseInt(br.readLine()); 
+		
+		Integer [] bookPricesInOrder = new Integer[booksPrices.size()];
+		bookPricesInOrder = booksPrices.toArray(bookPricesInOrder);
+		
+		
+		boolean found =false;
+	
+		//Los números del primer recorrido
+		int num1=0;
+		int num2=0;
+		int difference=0;
+		
+		int n1=0;
+		int n2=0;
+	
+		
+		
+		
+		int menor=0;
+		int nm1=0;
+		int nm2=0;
+		
+		
+		
+		for (int j=0;j<bookPricesInOrder.length && !found;j++) {
+			
+			num1 = bookPricesInOrder[j];
+			
+			for (int k=1;k<bookPricesInOrder.length && !found && k!=j;k++) {
+				
+				if (num1+ bookPricesInOrder[k]==money) {
+					found=true;
+					num2=bookPricesInOrder[k];
+					
+					if (num1>num2) {
+						difference= num1-num2;
+					}else {
+						difference = num2-num1;
+					}
+				}
+			}			
+		}
+		
+		
+		//System.out.println("Asignacion de num1= "+num1);
+		//System.out.println("Asignacion de num2= "+num2);
+		//System.out.println("Asignacion de difference= "+difference);
+		
+		for (int i=0;i<bookPricesInOrder.length ;i++) {
+			//System.out.println("***Entre al ciclo");
+			found=false;
+			int start =0;
+			int end=bookPricesInOrder.length-1;			
+			nm1= bookPricesInOrder[i];
+			
+			//System.out.println("Asignacion de num1= "+n1+"\n");
+			while (start<end && !found) {
+				//System.out.println("Estaba true pero lo cambie a false"+"\n");
+				int middle = (start+end)/2;
+				nm2=bookPricesInOrder[middle];
+				//System.out.println("Asignacion de middle= "+middle+"\n");
+				if (nm1+nm2>money) {
+					end=middle-1;
+					
+				}else if (nm1+nm2<money) {
+					start=middle+1;
+					
+				}else if (nm1+nm2==money) {
+					//System.out.println("***Encontre otra suma que da igual a money"+"\n");
+					found=true;
+					
+					
+					n1=nm1;
+					n2=nm2;	
+					
+					//System.out.println("++++Asignacion de nm1= "+nm1+"\n");
+					//System.out.println("++++Asignacion de nm2= "+nm2+"\n");
+					if (n1>n2) {
+						menor=n1-n2;
+					}else {
+						menor=n2-n1;
+					}					
+					
+					
+					if (menor<difference ) {
+						//n1=nm1;
+						//n2=nm2;
+						difference=menor;
+						output.add("Peter should buy books whose prices are "+n1+" and "+n2);
+						System.out.println("Peter should buy books whose prices are "+n1+" and "+n2);		
+					}
+				}
+			}
+			
+		}
+		
+		if (found == true) {
+			output.add("Peter should buy books whose prices are "+n1+" and "+n2);
+			System.out.println("Peter should buy books whose prices are "+n1+" and "+n2);			
+		}
+	}	
 
 }

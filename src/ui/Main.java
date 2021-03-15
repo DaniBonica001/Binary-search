4edeb771b65c37c8ee2d7b37f4a5d2330557b7e0
Daniela Bonilla Caceres <danielabc_0106@hotmail.com>
2021-03-15 14:15:38 -0500

    Commit: I fixed the output.

--- src/ui/Main.java
+++ src/ui/Main.java
@@ -79,6 +79,8 @@ public class Main {
 		for (int i=0;i<prices.size();i++) {
 			booksPrices.add(prices.get(i));
 		}	
+		//System.out.println(Arrays.toString(bookPrices));
+		
 	}
 		
 	public static void findBooks() {
@@ -100,6 +102,7 @@ public class Main {
 				int num1=0;
 				int num2=0;
 				int difference=0;
+				int d1=0;
 				
 				int n1=0;
 				int n2=0;
@@ -108,7 +111,9 @@ public class Main {
 				int nm1=0;
 				int nm2=0;
 				
-				
+				int a=0;
+				int b=0;
+				//int c=0;
 				
 				for (int j=0;j<bookPricesInOrder.length && !found;j++) {
 					
@@ -122,60 +127,115 @@ public class Main {
 							
 							if (num1>num2) {
 								difference= num1-num2;
-							}else {
+							}else if (num1<num2){
 								difference = num2-num1;
+							}else {
+								difference=num1-num2;
 							}
+							d1=difference;
 						}
 					}			
 				}
 				
-				boolean exit=false;
-								
-				for (int i=0;i<bookPricesInOrder.length && !exit ;i++) {
+				//System.out.println("***Asignación de num1: "+num1);
+				//System.out.println("***Asignación de num2: "+num2);
+				//System.out.println("***Asignación de difference: "+difference);
 				
-					found=false;
-					int start =0;
-					int end=bookPricesInOrder.length-1;			
-					nm1= bookPricesInOrder[i];
+				boolean exit=false;
+				boolean foundPrices=false;
+				int add=0;
+				for (int i=0;i<bookPricesInOrder.length && !exit;i++) {
 					
-					while (start<=end && !found) {
+					foundPrices=false;
+					int start=0;
+					int end=bookPricesInOrder.length-1;
+					nm1 = bookPricesInOrder[i];
+					//System.out.println("Asignacion nm1:"+nm1);
+										
+					while (start<=end && !foundPrices) {
 						
-						int middle = (start+end)/2;
-						nm2=bookPricesInOrder[middle];
-					
-						if (nm1+nm2>money) {
-							end=middle-1;
+						int middle=(start+end)/2;
+						
+						if (middle!=i) {
+							nm2 = bookPricesInOrder[middle];
+							//System.out.println("Asignacion nm2:"+nm2);
 							
-						}else if (nm1+nm2<money) {
-							start=middle+1;
-							
-						}else if (nm1+nm2==money) {
-							
-							found=true;						
-							
-							n1=nm1;
-							n2=nm2;	
-							
-							if (n1>n2) {
-								menor=n1-n2;
-							}else {
-								menor=n2-n1;
-							}					
-							
-							
-							if (menor<difference && menor!=0 ) {							
-								difference=menor;
-								output.add("Peter should buy books whose prices are "+n1+" and "+n2+".");										
-							}else if (menor==difference) {								
-								exit=true;
-								output.add("Peter should buy books whose prices are "+n1+" and "+n2+".");								
-							}else if (difference<menor) {								
-								exit=true;
-								output.add("Peter should buy books whose prices are "+num1+" and "+num2+".");
+							if (nm1+nm2>money) {
+								end=middle-1;
+								
+							}else if (nm1+nm2<money) {
+								start=middle+1;
+								
+							}else if (nm1+nm2==money) {
+								//System.out.println("Números que dan la suma");
+								foundPrices=true;
+								//String message;
+								n1=nm1;
+								n2=nm2;
+								
+								if (n1>n2) {
+									menor=n1-n2;
+									//message="Peter should buy books whose prices are "+n2+" and "+n1+".";
+								}else if (n2>n1) {
+									menor=n2-n1;
+									//message="Peter should buy books whose prices are "+n1+" and "+n2+".";
+								}else {
+									menor=n2-n1;
+									//message="Peter should buy books whose prices are "+n1+" and "+n2+".";
+								}
+								
+
+								if (menor<difference) {
+									difference=menor;
+									a=n1;
+									b=n2;									
+									add+=1;
+
+								}else if (d1<menor) {									
+									exit=true;
+									String dMessage="";
+									if (num1>num2) {
+										dMessage+="Peter should buy books whose prices are "+num2+" and "+num1+".";
+									}else if (num1<num2) {
+										dMessage+="Peter should buy books whose prices are "+num1+" and "+num2+".";
+									}else {
+										dMessage+="Peter should buy books whose prices are "+num1+" and "+num2+".";
+									}
+									output.add(dMessage);
+									
+																		
+								}else if (d1==menor) {
+									difference=d1;									
+								}
 							}
+							
+						}else if (middle==i) {
+							//System.out.println("middle es iguaal a i");
+							foundPrices=true;							
 						}
-					}					
+					}
 				}					
+				
+				if (menor==difference && add==0) {
+					String eMessage="";
+					if (num1>num2) {
+						eMessage+="Peter should buy books whose prices are "+num2+" and "+num1+".";
+					}else if (num1<num2) {
+						eMessage+="Peter should buy books whose prices are "+num1+" and "+num2+".";
+					}else {
+						eMessage+="Peter should buy books whose prices are "+num1+" and "+num2+".";
+					}
+					output.add(eMessage);
+				}else if (add!=0) {					
+					if (a<b) {
+						output.add("Peter should buy books whose prices are "+a+" and "+b+".");
+					}else if (a>b) {
+						output.add("Peter should buy books whose prices are "+b+" and "+a+".");
+					}else if (a==b) {
+						output.add("Peter should buy books whose prices are "+a+" and "+b+".");
+					}
+				}
+				
 			}
 			
 		}catch (IOException e) {
